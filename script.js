28441d24f19e66385c712946723df85cfab41201
Alessandro Mendonça <asm.dev81@gmail.com>
2024-03-20 17:10:29 -0300

    UPDATE

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function showDescription(id) {
-    var descriptions = document.querySelectorAll('.description');
-    descriptions.forEach(function(desc) {
-      desc.style.display = 'none';
-    });
-  
-    var description = document.getElementById(id);
-    if (description) {
-      description.style.display = 'block';
-    }
+window.onload = function() {
+  showDescription('description1');
+};
+
+ // Função para mostrar a descrição correspondente ao clicar em uma opção
+function showDescription(descriptionId) {
+  // Esconde todas as descrições
+  var descriptions = document.getElementsByClassName("description");
+  for (var i = 0; i < descriptions.length; i++) {
+      descriptions[i].style.display = "none";
   }
-  
+  // Mostra a descrição correspondente ao ID fornecido
+  document.getElementById(descriptionId).style.display = "block";
+}
